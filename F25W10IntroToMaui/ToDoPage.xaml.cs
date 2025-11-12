971b4a2182c3f0e6369722ce4fc31b89007109ea
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-11-12 14:44:40 -0500

    listview

---
+++ F25W10IntroToMaui/ToDoPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace F25W10IntroToMaui;
+
+public partial class ToDoPage : ContentPage
+{
+	public ToDoPage()
+	{
+		InitializeComponent();
+	}
+
+    private void btnAddTask_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
