8ead0170d24145a982bbd7c4187987d5c9ac6fe5
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-11-12 14:52:09 -0500

    add task

--- F25W10IntroToMaui/ToDoPage.xaml.cs
+++ F25W10IntroToMaui/ToDoPage.xaml.cs
@@ -1,14 +1,24 @@
+using System.Collections.ObjectModel;
+
 namespace F25W10IntroToMaui;
 
 public partial class ToDoPage : ContentPage
 {
+	private ObservableCollection<string> tasks = new ObservableCollection<string>();
+
 	public ToDoPage()
 	{
 		InitializeComponent();
+		lvTasks.ItemsSource = tasks;
 	}
 
     private void btnAddTask_Clicked(object sender, EventArgs e)
     {
-
+		if (!string.IsNullOrWhiteSpace(txtTask.Text))
+		{
+			tasks.Add(txtTask.Text);
+			txtTask.Text = "";
+			txtTask.Focus();
+		}
     }
 }
